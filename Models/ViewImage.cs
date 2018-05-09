0f2177b17208fedfd4ea628330a19b8aa0977831
Eric Dahlvang <v-erdahl@microsoft.com>
2018-05-08 19:38:28 -0700

    Add Razorback with WildlifeView and ProxyMessageView. (#11)

---
+++ Models/ViewImage.cs
@@ -0,0 +1,8 @@
+﻿namespace ContosoScuba.Bot.Models
+{
+    public class ViewImage
+    {
+        public string ImageUrl { get; set; }
+        public string OpenUrl { get; set; }
+    }
+}
---
+++ Models/WildlifeData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ContosoScuba.Bot.Models
+{
+
+    public class WildlifeData
+    {
+        public WildlifeData()
+        {
+            Images = new List<ViewImage>();
+        }
+        public string Introduction { get; set; }
+        public string Instructions { get; set; }
+        public List<ViewImage> Images { get; set; }
+    }
+}
